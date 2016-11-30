a1ddefc3abb864c6bd473ede7a164f2471209409
Desktop <mpwinters86@gmail.com>
2016-11-29 19:38:17 -0600

    Initial Check in

---
+++ One Clip Hero/One Clip Hero/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace One_Clip_Hero
+{
+    [Activity(Label = "One_Clip_Hero", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
